f2faed8cdf0bb3239bfa8403efb1ac61596a61f6
Skierhy <Skierhy@gmail.com>
2022-10-08 23:02:02 -0500

    comprimir imagen

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+import gulp from 'gulp';
+import imagemin from 'gulp-imagemin';
+
+export default () =>
+	gulp
+		.src('./images/heroes/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('./dist/images'));
